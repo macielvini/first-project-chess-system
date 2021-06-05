8ce3646ccf550da484ae28f61a7827946334db67
Vinicius Maciel <vmaciel1009@gmail.com>
2021-06-05 16:44:10 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,30 @@
 package application;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.err.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 }
