47675082ad3d01fbc47be5a2029b08d4e18e0a74
Vinicius Maciel <vmaciel1009@gmail.com>
2021-06-09 21:57:55 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-	
+
 	private char column;
 	private int row;
 
@@ -22,15 +22,15 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-	
+
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char) ('a'-position.getColumn()), 8-position.getRow());
+		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
 	}
-	
+
 	@Override
 	public String toString() {
 		return "" + column + row;
